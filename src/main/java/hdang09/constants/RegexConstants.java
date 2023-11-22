3ee26a2301a19e0436451ac2835c29906ba31ef8
hdang09 <dangtranhai628@gmail.com>
2023-11-22 21:26:11 +0700

    UPDATE - Update server URL and and student ID regex

--- src/main/java/hdang09/constants/RegexConstants.java
+++ src/main/java/hdang09/constants/RegexConstants.java
@@ -2,5 +2,5 @@ package hdang09.constants;
 
 public interface RegexConstants {
     String PHONE_NUMBER_REGEX = "^(0|\\+?84)[35789][0-9]{8}$";
-    String STUDENT_ID_REGEX = "^[SsHhDd][EeAaSs]+([0-9]{6})$";
+    String STUDENT_ID_REGEX = "^[SsHhDdQqCc][EeAaSs]+([0-9]{6})$";
 }
