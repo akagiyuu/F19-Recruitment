89f47f7b4f9fc7015746a75030b398f5204b7bfa
AnataArisa <92080339+AnataAria@users.noreply.github.com>
2024-10-30 01:57:14 +0700

    Fix cors

--- src/main/java/tech/fcode/recruitment/configs/CorsConfig.java
+++ src/main/java/tech/fcode/recruitment/configs/CorsConfig.java
@@ -7,14 +7,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+    @Bean
+    WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @SuppressWarnings("null")
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("Accept",
+                                "Access-Control-Allow-Headers",
+                                "Access-Control-Allow-Methods",
+                                "Access-Control-Allow-Origin",
+                                "Authorization",
+                                "Content-Type",
+                                "Origin",
+                                "X-Requested-With")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH")
+                        .exposedHeaders("Accept",
+                                "Access-Control-Allow-Headers",
+                                "Access-Control-Allow-Methods",
+                                "Access-Control-Allow-Origin",
+                                "Authorization",
+                                "Content-Type",
+                                "Origin",
+                                "X-Requested-With");
+            }
+        };
+    }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
                 "Accept",
                 "Access-Control-Allow-Headers",
